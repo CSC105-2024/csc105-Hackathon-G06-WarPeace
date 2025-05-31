a4d5e83fdd5d9683a709140ecd8a52e8dd4ab912
Seamew <yutsuzetime@gmail.com>
2025-05-31 15:26:16 +0700

    update login

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -7,17 +7,26 @@ function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        if (!username || !password) {
+            alert("Please enter both username and password.");
+            return;
+        }
+        nav("/homepage"); 
+    };
     return (
         <div className="min-h-screen bg-[#2D3138] flex items-center justify-center px-4">
             <div className=" text-white p-8 rounded-md w-full max-w-md sm:max-w text-center 2xl:">
 
                 <img src={logo} className="text-shadow " />
                 <p className="text-lg lg:text-xl font-shadow font-bold text-[#F04E23] my-6">Say What You Mean—Respectfully</p>
-                <form className="space-y-4">
+                <form onSubmit={handleSubmit} className="space-y-4">
                     <div>
                         <p className="text-white text-left lg:text-xl mb-2">Username</p>
                         <input
                             type="text"
+                            value={username}
                             className="w-full px-4 py-2 text-white bg-black rounded-[3px] focus:outline-none  "
                         />
                     </div>
@@ -25,6 +34,7 @@ function Login() {
                         <p className="text-white text-left lg:text-xl mb-2">Password</p>
                         <input
                             type="password"
+                            value={password}
                             className="w-full px-4 py-2 text-white bg-black rounded-[3px]  focus:outline-none     "
                         />
                     </div>
