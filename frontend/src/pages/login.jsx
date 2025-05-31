e9856835c69c7d77562d18f7a1a6e56743b6089d
Seamew <yutsuzetime@gmail.com>
2025-05-31 15:14:49 +0700

    Register + zod

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -5,33 +5,33 @@ function Login() {
     const nav = useNavigate();
     return (
         <div className="min-h-screen bg-[#2D3138] flex items-center justify-center px-4">
-            <div className=" text-white p-8 rounded-md w-full max-w-xl text-center ">
+            <div className=" text-white p-8 rounded-md w-full max-w-md sm:max-w text-center 2xl:">
 
-                <img src={logo} className=""/>
-                <p className="text-sm lg:text-lg font-bold text-[#F04E23] my-6">Say What You Mean—Respectfully</p>
+                <img src={logo} className="text-shadow "/>
+                <p className="text-lg lg:text-xl font-shadow font-bold text-[#F04E23] my-6">Say What You Mean—Respectfully</p>
                 <form className="space-y-4">
                     <div>
-                        <p className="text-white text-left">Username</p>
+                        <p className="text-white text-left lg:text-xl mb-2">Username</p>
                         <input
                             type="text"
-                            className="w-full px-4 py-2 text-white bg-black rounded-3 focus:outline-none"
+                            className="w-full px-4 py-2 text-white bg-black rounded-[3px] focus:outline-none  "
                         />
                     </div>
                     <div>
-                        <p className="text-white text-left">Password</p>
+                        <p className="text-white text-left lg:text-xl mb-2">Password</p>
                         <input
                             type="password"
-                            className="w-full px-4 py-2 text-white bg-black rounded-3 focus:outline-none"
+                            className="w-full px-4 py-2 text-white bg-black rounded-[3px]  focus:outline-none     "
                         />
                     </div>
                     <button
                         type="submit"
-                        className="w-24 bg-yellow-500 hover:bg-yellow-400 cursor-pointer text-black font-bold py-2 mt-4 rounded-3"
+                        className="w-24 bg-yellow-500 hover:bg-yellow-400 cursor-pointer rounded-[3px]  text-black font-bold py-2 mt-4 rounded-3"
                     >
                         Login
                     </button>
                 </form>
-                <p className="text-sm mt-4">
+                <p className="lg:text-lg mt-4 ">
                     Don't have an account? <button onClick={()=>nav("/signUp")} className="underline font-bold cursor-pointer">Sign up</button>
                 </p>
 
